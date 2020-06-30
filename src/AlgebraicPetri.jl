0d7501c9bec64c766e5d40402150f8dafa018e7d
Micah Halter <micah.halter@gtri.gatech.edu>
2020-06-30 14:59:20 -0400

    Added all functions except composition and otimes of petricospan objects

--- src/AlgebraicPetri.jl
+++ src/AlgebraicPetri.jl
@@ -9,9 +9,9 @@ using Catlab.CategoricalAlgebra.ShapeDiagrams
 using Catlab.CategoricalAlgebra.FinSets
 using Petri
 
-import Catlab.Theories: dom, codom, id, compose, ⋅, ∘, otimes, munit, braid,
-                        mcopy, mmerge, create, delete, pair, copair, proj1, 
-                        proj2, coproj1, coproj2
+import Catlab.Theories: dom, codom, id, compose, ⋅, ∘, otimes, ⊗, munit,
+                        braid, σ, mcopy, Δ, mmerge, ∇, create, □, delete, ◊,
+                        pair, copair, proj1, proj2, coproj1, coproj2
 
 struct PetriCospanOb
     n::Int
@@ -45,6 +45,9 @@ NullPetri(n::Int) = Petri.Model(collect(1:n), Vector{Tuple{Vector{Int}, Vector{I
     codom(f::PetriCospan) = PetriCospanOb(dom(right(f.f)).n)
 
     compose(f::PetriCospan, g::PetriCospan) = begin
+        # if you have PetriCospans f: x -> a <- y and g: y -> b <- z
+        # solve pushout of the span defined as a <- y -> b, to get a +_y b
+        # figure out PetriFunctor for a +_y b
         Nothing
     end
 
@@ -65,24 +68,38 @@ NullPetri(n::Int) = Petri.Model(collect(1:n), Vector{Tuple{Vector{Int}, Vector{I
     munit(::Type{PetriCospanOb}) = PetriCospanOb(0)
 
     braid(X::PetriCospanOb, Y::PetriCospanOb) = begin
-        Nothing
+        Z = otimes(X, Y)
+        PetriCospan(DecoratedCospan(
+            Cospan(
+                id(FinOrd(Z.n)),
+                FinOrdFunction(x->x > X.n ? x - X.n : x + Y.n, Z.n, Z.n)
+            ), id(PetriFunctor, Z.n), NullPetri(Z.n)
+        ))
     end
 
-    mcopy(X::PetriCospanOb) = begin
-        Nothing
-    end
+    mcopy(X::PetriCospanOb) = PetriCospan(DecoratedCospan(
+        Cospan(
+            id(FinOrd(X.n)),
+            FinOrdFunction(x->x > X.n ? x - X.n : x, 2*X.n, X.n)
+        ), id(PetriFunctor, X.n), NullPetri(X.n)
+    ))
 
-    mmerge(X::PetriCospanOb) = begin
-        Nothing
-    end
+    mmerge(X::PetriCospanOb) = PetriCospan(DecoratedCospan(
+        Cospan(
+            FinOrdFunction(x->x > X.n ? x - X.n : x, 2*X.n, X.n),
+            id(FinOrd(X.n))
+        ), id(PetriFunctor, X.n), NullPetri(X.n)
+    ))
 
-    create(X::PetriCospanOb) = begin
-        Nothing
-    end
+    create(X::PetriCospanOb) = PetriCospan(DecoratedCospan(
+        Cospan(FinOrdFunction(x->Nothing, 0, X.n), id(FinOrd(X.n))),
+        id(PetriFunctor, X.n), NullPetri(X.n)
+    ))
 
-    delete(X::PetriCospanOb) = begin
-        Nothing
-    end
+    delete(X::PetriCospanOb) = PetriCospan(DecoratedCospan(
+        Cospan(id(FinOrd(X.n)), FinOrdFunction(x->Nothing, 0, X.n)),
+        id(PetriFunctor, X.n), NullPetri(X.n)
+    ))
 
     pair(f::PetriCospan, g::PetriCospan) = compose(mcopy(dom(f)), otimes(f, g))
     copair(f::PetriCospan, g::PetriCospan) = compose(otimes(f, g), mmerge(codom(f)))
