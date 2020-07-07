19b9c20bc1dcb08781706d8524abd3b1e7d98c15
Micah Halter <micah.halter@gtri.gatech.edu>
2020-07-07 08:44:04 -0400

    Updated to new examples documentation

--- examples/covid/covid.jl
+++ examples/covid/covid.jl
@@ -1,24 +1,25 @@
-# -*- coding: utf-8 -*-
-# + {}
+# # [Multi-City COVID-19 Model](@id covid_example)
+#
+#md # [![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/examples/covid/covid.ipynb)
+
+using AlgebraicPetri
+
 using Petri
 using OrdinaryDiffEq
 using Plots
-using AlgebraicPetri
+
 using Catlab
 using Catlab.Theories
 using Catlab.Programs
 using Catlab.WiringDiagrams
 using Catlab.CategoricalAlgebra.ShapeDiagrams
-using Catlab.CategoricalAlgebra.FinSets
 using Catlab.Graphics
 using Catlab.Graphics.Graphviz: Graph
 
 import Catlab.Theories: id
 
-# A few helper functions
-display_wd(ex) = to_graphviz(ex, orientation=LeftToRight, labels=true)
+display_wd(ex) = to_graphviz(ex, orientation=LeftToRight, labels=true);
 id(args...) = foldl((x,y)->id(x) ⊗ id(y), args);
-# -
 
 # #### Step 1: Define building block Petri Net models
 
@@ -36,7 +37,6 @@ travel_petri = PetriCospan(
 
 # #### Step 2: Define a strongly type presentation of the Free Biproduct Category for the desired domain
 
-# +
 @present EpidemiologyWithTravel(FreeBiproductCategory) begin
     S::Ob
     E::Ob
@@ -49,17 +49,14 @@ travel_petri = PetriCospan(
     recovery::Hom(I,R)
     death::Hom(I,D)
     travel::Hom(S⊗E⊗I,S⊗E⊗I)
-end
+end;
 
-# Create the generators
-S,E,I,R,D,transmission,exposure,illness,recovery,death,travel = generators(EpidemiologyWithTravel)
+S,E,I,R,D,transmission,exposure,illness,recovery,death,travel = generators(EpidemiologyWithTravel);
 
-# Define a functor from the generators to the building block Petri Nets
 F(ex) = functor((PetriCospanOb, PetriCospan), ex, generators=Dict(
         S=>ob, E=>ob, I=>ob, R=>ob, D=>ob,
         transmission=>transmission_petri, exposure=>exposure_petri,
         illness=>spontaneous_petri, recovery=>spontaneous_petri, death=>spontaneous_petri,travel=>travel_petri));
-# -
 
 # #### Step 3: Create, visualize, and solve model
 
@@ -67,11 +64,12 @@ F(ex) = functor((PetriCospanOb, PetriCospan), ex, generators=Dict(
 #
 # SEIRD City Model with travel between cities as $S ⊗ E ⊗ I → S ⊗ E ⊗ I$
 #
-# $seird_{city} = (((Δ(S) ⊗ id(E)) ⋅ (id(S) ⊗ σ(S,E))) ⊗ id(I)) ⋅ (id(S, E) ⊗ exposure) ⋅ (id(S) ⊗ (∇(E) ⋅ Δ(E)) ⊗ id(I)) ⋅ (id(S, E) ⊗ ((illness ⊗ id(I)) ⋅ (∇(I) ⋅ Δ(I)) ⋅ (id(I) ⊗ (Δ(I) ⋅(recovery ⊗ death))))) ⋅ (travel ⊗ ◊(R) ⊗ ◊(D))$
-#       
+# $seird_{city} = (((Δ(S) ⊗ id(E)) ⋅ (id(S) ⊗ σ(S,E))) ⊗ id(I)) ⋅ (id(S, E) ⊗ exposure) ⋅
+#                 (id(S) ⊗ (∇(E) ⋅ Δ(E)) ⊗ id(I)) ⋅ (id(S, E) ⊗ ((illness ⊗ id(I)) ⋅
+#                 (∇(I) ⋅ Δ(I)) ⋅ (id(I) ⊗ (Δ(I) ⋅(recovery ⊗ death))))) ⋅ (travel ⊗ ◊(R) ⊗ ◊(D))$
+#
 # This is a very complicated interaction, so we can use the program interface for easier model definition
 
-# +
 seird_city = @program EpidemiologyWithTravel (s::S, e::E, i::I) begin
     e2, i2 = exposure(s, i)
     i3 = illness(e2)
@@ -82,40 +80,40 @@ end
 seird_city = to_hom_expr(FreeBiproductCategory, seird_city)
 
 display_wd(seird_city)
-# -
-
+#-
 Graph(decoration(F(seird_city)))
 
-# +
-# function to compose n city models together
-ncities(city,n::Int) = compose([city for i in 1:n]...)
+# create a multi-city SEIRD models
 
-# create a 3 city SEIRD models
+ncities(city,n::Int) = compose([city for i in 1:n]...);
+
 seird_3 = ncities(seird_city, 3)
 pc_seird_3 = F(seird_3)
 p_seird_3 = decoration(pc_seird_3)
-display(display_wd(seird_3))
-display(Graph(p_seird_3))
-# -
+display_wd(seird_3)
+#-
+Graph(p_seird_3)
 
-# Define time frame, 2 months
-tspan = (0.0,60.0)
-# Define initial states
-u0 = zeros(Float64, base(pc_seird_3).n)
-u0[1]  = 10000
-u0[6]  = 10000
-u0[11] = 10000
-u0[2]  = 1
-# Define transition rates
+# Define time frame and initial parameters
+
+tspan = (0.0,60.0);
+u0 = zeros(Float64, base(pc_seird_3).n);
+u0[1]  = 10000;
+u0[6]  = 10000;
+u0[11] = 10000;
+u0[2]  = 1;
+
 seirdparams(n::Int, k::Number) = begin
     βseird = [10/sum(u0), 1/2, 1/5, 1/16]
     βtravel = [1/2, 1/200, 1/2]/100k
     β = vcat(βseird, βtravel)
     return foldl(vcat, [β for i in 1:n])
-end
-params = seirdparams(3, 2)
-# Generate and solve resulting ODE
-prob = ODEProblem(toODE(p_seird_3),u0,tspan,params)
-sol = OrdinaryDiffEq.solve(prob,Tsit5())
-# visualize the solution
+end;
+params = seirdparams(3, 2);
+
+# Generate, solve, and visualize resulting ODE
+
+prob = ODEProblem(toODE(p_seird_3),u0,tspan,params);
+sol = OrdinaryDiffEq.solve(prob,Tsit5());
+
 plot(sol)
