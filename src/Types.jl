5e1b5491250d08765b12cd68b76fd3d5381f7975
Owen Lynch <root@owenlynch.org>
2020-07-30 17:28:43 -0400

    ENH: Some cleanup of basic Petri net implementation

--- src/Types.jl
+++ src/Types.jl
@@ -1,4 +1,8 @@
-module Petri
+module Types
+export TheoryPetri, Petri, AbstractPetri, ns, nt, ni, no,
+  add_species!, add_transition!, add_transitions!,
+  add_input!, add_inputs!, add_output!, add_outputs!, inputs, outputs,
+  TheoryReactionNet, ReactionNet, AbstractReactionNet
 
 using Catlab
 using Catlab.CategoricalAlgebra.CSets
@@ -9,15 +13,15 @@ using Catlab.Theories
 ############
 
 @present TheoryPetri(FreeCategory) begin
-    T::Ob
-    S::Ob
-    I::Ob
-    O::Ob
+  T::Ob
+  S::Ob
+  I::Ob
+  O::Ob
 
-    it::Hom(I,T)
-    is::Hom(I,S)
-    ot::Hom(O,T)
-    os::Hom(O,S)
+  it::Hom(I,T)
+  is::Hom(I,S)
+  ot::Hom(O,T)
+  os::Hom(O,S)
 end
 
 const AbstractPetri = AbstractCSetType(TheoryPetri)
@@ -43,56 +47,54 @@ add_output!(p::AbstractPetri,n,t,s) = add_parts!(p,:O,n,(os=s,ot=t))
 # Note: although indexing makes this pretty fast, it is often faster to bulk-convert
 # the Petri net into a transition matrix, if you are working with all of the transitions
 inputs(p::AbstractPetri,t) = subpart(p,incident(p,t,:it),:is)
-outputs(p::AbstractPetri,t) = subpart(p,incident(p,t,:ot),os)
+outputs(p::AbstractPetri,t) = subpart(p,incident(p,t,:ot),:os)
 
-totransmat(p::AbstractPetri) = begin
-    A = zeros(Int64,(nt(p),ns(p),2))
+struct TransitionMatrices
+  input::Matrix{Int}
+  output::Matrix{Int}
+  TransitionMatrices(p::AbstractPetri) = begin
+    input, output = zeros(Int,(nt(p),ns(p))), zeros(Int,(nt(p),ns(p)))
     for i in 1:ni(p)
-        A[subpart(p,i,:it),subpart(p,i,:is),1] += 1
+      input[subpart(p,i,:it),subpart(p,i,:is)] += 1
     end
     for i in 1:no(p)
-        A[subpart(p,i,:ot),subpart(p,o,:os),2] += 1
+      output[subpart(p,i,:ot),subpart(p,o,:os)] += 1
     end
-    A
+    new(input,output)
+  end
 end
 
-fromtransmat(A::Array{Int64,3}) = begin
-    (m,n,_) = size(A)
-    p = Petri()
-    add_species!(p,n)
-    add_transitions!(p,m)
-    for i in 1:m
-        for j in 1:n
-            add_inputs!(p,A[i,j,1],i,j)
-            add_outputs!(p,A[i,j,2],i,j)
-        end
+Petri(tm::TransitionMatrices) =
+  (m,n) = size(tm.input)
+  p = Petri()
+  add_species!(p,n)
+  add_transitions!(p,m)
+  for i in 1:m
+    for j in 1:n
+      add_inputs!(p,tm.input[i,j],i,j)
+      add_outputs!(p,tm.output[i,j],i,j)
     end
-    p
+  end
+  p
 end
 
 rate_eq(p::AbstractPetri) = begin
-    n,m = nt(p),ns(p)
-    rates = zeros(m)
-    A = totransmat(p)
-    f(du,u,p,t) = begin
-        for i in 1:n
-            rates[i] = p[i] * prod(u[j] ^ T[i,j,1] for j in 1:m)
-        end
-        for j in 1:m
-            du[j] = sum(rates[i] * (T[i,j,2] - T[i,j,1]) for i in 1:n)
-        end
-    end
-    f
+  tm = TransitionMatrices(p)
+  f(du,u,p,t) = begin
+    log_rates = log.(p) .+ tm.input * log.(u)
+    du = (tm.output - tm.input) * exp.(log_rates)
+  end
+  f
 end
 
 # Reaction Nets
 ###############
 
 @present TheoryReactionNet <: TheoryPetri begin
-    number::Ob
+  number::Ob
 
-    rate::Hom(T, number)
-    u0::Hom(S, number)
+  rate::Hom(T, number)
+  u0::Hom(S, number)
 end
 
 const AbstractReactionNet = AbstractCSetType(TheoryReactionNet, data=[:number])
