a6d96c9d2405059518a3ff09a874861fa26874fd
Micah Halter <micah.halter@gtri.gatech.edu>
2020-09-04 13:02:21 -0400

    Added better constructor for building petri nets

--- src/Types.jl
+++ src/Types.jl
@@ -2,6 +2,7 @@ module Types
 export TheoryPetri, Petri, AbstractPetri, ns, nt, ni, no,
   add_species!, add_transition!, add_transitions!,
   add_input!, add_inputs!, add_output!, add_outputs!, inputs, outputs,
+  TransitionMatrices, rate_eq,
   TheoryReactionNet, ReactionNet, AbstractReactionNet
 
 using Catlab
@@ -28,22 +29,35 @@ end
 const AbstractPetri = AbstractCSet{CatDescType(TheoryPetri)}
 const Petri = CSet{CatDescType(TheoryPetri),(:is,:it,:os,:ot)}
 
+(pt::typeof(Petri))(n,ts...) = begin
+  p = pt()
+  add_species!(p, n)
+  add_transitions!(p, length(ts))
+  for (i,(ins,outs)) in enumerate(ts)
+    if length(ins) == 1 ins = [ins] end
+    if length(outs) == 1 outs = [outs] end
+    add_inputs!(p, length(ins), repeat([i], length(ins)), collect(ins))
+    add_outputs!(p, length(outs), repeat([i], length(outs)), collect(outs))
+  end
+  p
+end
+
 ns(p::AbstractPetri) = nparts(p,:S)
 nt(p::AbstractPetri) = nparts(p,:T)
 ni(p::AbstractPetri) = nparts(p,:I)
 no(p::AbstractPetri) = nparts(p,:O)
 
-add_species!(p::AbstractPetri) = add_part!(p,:S)
+add_species!(p::AbstractPetri;kw...) = add_part!(p,:S;kw...)
 add_species!(p::AbstractPetri,n) = add_parts!(p,:S,n)
 
 add_transition!(p::AbstractPetri) = add_part!(p,:T)
 add_transitions!(p::AbstractPetri,n) = add_parts!(p,:T,n)
 
-add_input!(p::AbstractPetri,t,s) = add_part!(p,:I;is=s,it=t)
-add_inputs!(p::AbstractPetri,n,t,s) = add_parts!(p,:I,n;is=s,it=t)
+add_input!(p::AbstractPetri,t,s) = add_part!(p,:I;it=t,is=s)
+add_inputs!(p::AbstractPetri,n,t,s) = add_parts!(p,:I,n;it=t,is=s)
 
-add_output!(p::AbstractPetri,t,s) = add_part!(p,:O;os=s,ot=t)
-add_output!(p::AbstractPetri,n,t,s) = add_parts!(p,:O,n;os=s,ot=t)
+add_output!(p::AbstractPetri,t,s) = add_part!(p,:O;ot=t,os=s)
+add_outputs!(p::AbstractPetri,n,t,s) = add_parts!(p,:O,n;ot=t,os=s)
 
 # Note: although indexing makes this pretty fast, it is often faster to bulk-convert
 # the Petri net into a transition matrix, if you are working with all of the transitions
@@ -58,8 +72,8 @@ struct TransitionMatrices
     for i in 1:ni(p)
       input[subpart(p,i,:it),subpart(p,i,:is)] += 1
     end
-    for i in 1:no(p)
-      output[subpart(p,i,:ot),subpart(p,o,:os)] += 1
+    for o in 1:no(p)
+      output[subpart(p,o,:ot),subpart(p,o,:os)] += 1
     end
     new(input,output)
   end
