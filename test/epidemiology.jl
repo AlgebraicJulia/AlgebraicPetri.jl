513d9b203bd3648bb5198e57077bcb476b98e6cb
Micah Halter <micah.halter@gtri.gatech.edu>
2020-11-06 18:38:56 -0500

    Added more tests for coverage

--- test/epidemiology.jl
+++ test/epidemiology.jl
@@ -3,3 +3,10 @@ sir_petri = LabelledPetriNet([:S,:I,:R], :inf=>((:S,:I)=>(:I,:I)), :rec=>(:I=>:R
 sir = infection ⋅ recovery
 
 @test apex(sir) == sir_petri
+
+sir_relation = @relation (s,i,r) begin
+    infection(s,i)
+    recovery(i,r)
+end
+
+@test apex(oapply_epi(sir_relation)) == sir_petri
--- test/petri.jl
+++ test/petri.jl
@@ -17,3 +17,21 @@ h_id = h ⋅ id(OpenPetriNetOb(FinSet(1)))
 
 @test h == h′
 @test h == h_id
+
+# Test open petri net notations either fully open or by specifying legs
+
+pn = Open(PetriNet(3, ((1,2), 3)), [1], [2], [3])
+pn′ = Open(PetriNet(3, ((1,2), 3)))
+@test pn == pn′
+
+lpn = Open(LabelledPetriNet([:I, :R], (:rec, :I=>:R)))
+lpn′ = Open([:I], LabelledPetriNet([:I, :R], (:rec, :I=>:R)), [:R])
+@test lpn == lpn′
+
+rn = Open(ReactionNet{Number,Int}([10, 0], (.25, 1=>2)))
+rn′ = Open([1], ReactionNet{Number,Int}([10, 0], (.25, 1=>2)), [2])
+@test rn == rn′
+
+lrn = Open(LabelledReactionNet{Number,Int}([:I=>10, :R=>0], ((:rec=>.25), :I=>:R)))
+lrn′ = Open([:I], LabelledReactionNet{Number,Int}([:I=>10, :R=>0], ((:rec=>.25), :I=>:R)), [:R])
+@test lrn == lrn′
