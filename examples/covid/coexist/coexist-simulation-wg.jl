7e3fedbd59c95b9cd16625c19ed4d1caa8135375
bosonbaas <bosonbaas@gmail.com>
2021-01-13 13:02:02 -0500

    Added some docstrings and centralized code

--- examples/covid/coexist/coexist-simulation-wg.jl
+++ examples/covid/coexist/coexist-simulation-wg.jl
@@ -27,6 +27,78 @@ end
 # reactions in an epidemiology Model. Either a state
 # spontaneously changes, or one state causes another to change
 
+using Catlab.Graphs.BasicGraphs
+function cross_uwd(g::Catlab.Graphs.BasicGraphs.Graph)
+    rel = RelationDiagram{Symbol}(0)
+    
+    # Add populations
+    juncs = add_junctions!(rel, nv(g), variable=[Symbol("pop$i") for i in 1:nv(g)])
+    srcs = subpart(g, :src)
+    tgts = subpart(g, :tgt)
+    # Add cross boxes
+    add_parts!(rel, :Box, ne(g), name=[Symbol("cross_$(srcs[i])_$(tgts[i])") for i in 1:ne(g)])
+    add_parts!(rel, :Port, ne(g), junction=srcs, box=1:ne(g))
+    add_parts!(rel, :Port, ne(g), junction=tgts, box=1:ne(g))
+    
+    # Add epidemiology model boxes
+    boxes = add_parts!(rel, :Box, nv(g), name=[Symbol("ep$i") for i in 1:nv(g)])
+    add_parts!(rel, :Port, nv(g), junction=juncs, box=boxes)
+    rel 
+end
+
+function stratify(epi_petri::Function, connection_graph::Catlab.Graphs.BasicGraphs.Graph, diffusion_petri::Function)
+    conn_uwd = cross_uwd(connection_graph)
+    
+    # Calls diffusion_petri for each edge as (src, tgt)
+    ep_map = Dict([Symbol("ep$i")=>epi_petri(i) for i in 1:nv(connection_graph)])
+    srcs = subpart(connection_graph, :src)
+    tgts = subpart(connection_graph, :tgt)
+    for i in 1:ne(connection_graph)
+        ep_map[Symbol("cross_$(srcs[i])_$(tgts[i])")] = diffusion_petri(srcs[i], tgts[i])
+    end
+    
+    oapply(conn_uwd, ep_map)
+end
+
+dem_connection(epi_model::Function, sus_state::Symbol, exp_state::Symbol, inf_states::Array{Symbol}, x::Int, y::Int) = begin
+    append_ind(x::Symbol, ind::Int) = Symbol("$(x)_$ind")
+    
+    ep1 = apex(epi_model(x))
+    ep2 = apex(epi_model(y))
+    
+    sus1 = append_ind(sus_state, x)
+    sus2 = append_ind(sus_state, y)
+    exp1 = append_ind(exp_state, x)
+    exp2 = append_ind(exp_state, y)
+    inf1 = [append_ind(inf, x) for inf in inf_states]
+    inf2 = [append_ind(inf, y) for inf in inf_states]
+    
+    LabelledPetriNet(vcat(subpart(ep1, :sname), subpart(ep2, :sname)),
+                     vcat([Symbol("crx_$(sus1)_$(inf)")=>((sus1, inf)=>(inf, exp1)) for inf in inf2],
+                          [Symbol("crx_$(sus2)_$(inf)")=>((sus2, inf)=>(inf, exp2)) for inf in inf1])...)
+    
+end
+
+diff_connection(epi_model::Function, x::Int, y::Int) = begin
+    ep1 = apex(epi_model(x))
+    ep2 = apex(epi_model(y))
+    states1 = subpart(ep1, :sname)
+    states2 = subpart(ep2, :sname)
+    LabelledPetriNet(vcat(states1, states2),
+                     vcat([Symbol("diff_$(states1[i])_$(states2[i])")=>(states1[i]=>states2[i]) for i in 1:ns(ep1)],
+                          [Symbol("diff_$(states2[i])_$(states1[i])")=>(states2[i]=>states1[i]) for i in 1:ns(ep2)])...)
+end
+
+add_index(epi_model::LabelledPetriNet, ind::Int) = begin
+    new_petri = deepcopy(epi_model)
+    snames = subpart(epi_model, :sname)
+    tnames = subpart(epi_model, :tname)
+    set_subpart!(new_petri, :sname, [Symbol("$(name)_$ind") for name in snames])
+    set_subpart!(new_petri, :tname, [Symbol("$(name)_$ind") for name in tnames])
+    new_petri
+end
+
+
 ob(x::Symbol) = codom(Open([x], LabelledPetriNet(x), [x])).ob;
 function spontaneous_petri(transition::Symbol, s::Symbol, t::Symbol)
     Open(LabelledPetriNet(unique([s, t]), transition=>(s=>t)))
@@ -38,18 +110,18 @@ end;
 # Define an `oapply` function that connects the building block Petri nets to
 # the operations we will use in the model.
 
-F(ex, n) = oapply(ex, Dict(
-    :exposure=>exposure_petri(Symbol(:exp_, n), Symbol(:S,n), Symbol(:I,n), Symbol(:E,n)),
-    :exposure_e=>exposure_petri(Symbol(:exp_e, n), Symbol(:S,n), Symbol(:E,n),Symbol(:E,n)),
-    :exposure_i2=>exposure_petri(Symbol(:exp_i2, n), Symbol(:S,n), Symbol(:I2,n), Symbol(:E,n)),
-    :exposure_a=>exposure_petri(Symbol(:exp_a, n), Symbol(:S,n), Symbol(:A,n),Symbol(:E,n)),
-    :progression=>spontaneous_petri(Symbol(:prog_, n), Symbol(:I,n), Symbol(:I2,n)),
-    :asymptomatic_infection=>spontaneous_petri(Symbol(:asymp_, n), Symbol(:E,n), Symbol(:A,n)),
-    :illness=>spontaneous_petri(Symbol(:ill_, n), Symbol(:E,n), Symbol(:I,n)),
-    :asymptomatic_recovery=>spontaneous_petri(Symbol(:arec_, n), Symbol(:A,n), Symbol(:R,n)),
-    :recovery=>spontaneous_petri(Symbol(:rec_, n), Symbol(:I2,n), Symbol(:R,n)),
-    :recover_late=>spontaneous_petri(Symbol(:rec2_, n), Symbol(:R,n), Symbol(:R2,n)),
-    :death=>spontaneous_petri(Symbol(:death2_, n), Symbol(:I2,n), Symbol(:D,n))));
+F(ex) = oapply(ex, Dict(
+    :exposure=>exposure_petri(Symbol(:exp_), Symbol(:S), Symbol(:I), Symbol(:E)),
+    :exposure_e=>exposure_petri(Symbol(:exp_e), Symbol(:S), Symbol(:E),Symbol(:E)),
+    :exposure_i2=>exposure_petri(Symbol(:exp_i2), Symbol(:S), Symbol(:I2), Symbol(:E)),
+    :exposure_a=>exposure_petri(Symbol(:exp_a), Symbol(:S), Symbol(:A),Symbol(:E)),
+    :progression=>spontaneous_petri(Symbol(:prog_), Symbol(:I), Symbol(:I2)),
+    :asymptomatic_infection=>spontaneous_petri(Symbol(:asymp_), Symbol(:E), Symbol(:A)),
+    :illness=>spontaneous_petri(Symbol(:ill_), Symbol(:E), Symbol(:I)),
+    :asymptomatic_recovery=>spontaneous_petri(Symbol(:arec_), Symbol(:A), Symbol(:R)),
+    :recovery=>spontaneous_petri(Symbol(:rec_), Symbol(:I2), Symbol(:R)),
+    :recover_late=>spontaneous_petri(Symbol(:rec2_), Symbol(:R), Symbol(:R2)),
+    :death=>spontaneous_petri(Symbol(:death2_), Symbol(:I2), Symbol(:D))));
 
 # Define the COEXIST model using the `@relation` macro
 
@@ -67,38 +139,38 @@ coexist = @relation (s, e, i, i2, a, r, r2, d) begin
     recover_late(r, r2)
 end;
 display_uwd(coexist)
-Graph(F(coexist, 1))
+AlgebraicPetri.Graph(F(coexist))
 
 # Define an `oapply` function that can be used to create a model of
 # cross exposure between two sets of populations
 
-F_cx(ex, x,y) = oapply(ex, Dict(
-    :exposure=>exposure_petri(Symbol(:exp_, x,y), Symbol(:S,x), Symbol(:I,y), Symbol(:E,x)),
-    :exposure_e=>exposure_petri(Symbol(:exp_e, x,y), Symbol(:S,x), Symbol(:E,y),Symbol(:E,x)),
-    :exposure_a=>exposure_petri(Symbol(:exp_a, x,y), Symbol(:S,x), Symbol(:A,y),Symbol(:E,x)),
-    :exposure_i2=>exposure_petri(Symbol(:exp_i2, x,y), Symbol(:S,x), Symbol(:I2,y), Symbol(:E,x)),
-    :exposure′=>exposure_petri(Symbol(:exp_, y,x), Symbol(:S,y), Symbol(:I,x), Symbol(:E,y)),
-    :exposure_e′=>exposure_petri(Symbol(:exp_e, y,x), Symbol(:S,y), Symbol(:E,x),Symbol(:E,y)),
-    :exposure_a′=>exposure_petri(Symbol(:exp_a, y,x), Symbol(:S,y), Symbol(:A,x),Symbol(:E,y)),
-    :exposure_i2′=>exposure_petri(Symbol(:exp_i2, y,x), Symbol(:S,y), Symbol(:I2,x), Symbol(:E,y))
+F_cx(ex) = oapply(ex, Dict(
+    :exposure=>exposure_petri(Symbol(:exp_), Symbol(:S), Symbol(:I), Symbol(:E)),
+    :exposure_e=>exposure_petri(Symbol(:exp_e), Symbol(:S), Symbol(:E),Symbol(:E)),
+    :exposure_a=>exposure_petri(Symbol(:exp_a), Symbol(:S), Symbol(:A),Symbol(:E)),
+    :exposure_i2=>exposure_petri(Symbol(:exp_i2), Symbol(:S), Symbol(:I2), Symbol(:E)),
+    :exposure′=>exposure_petri(Symbol(:exp_), Symbol(:S), Symbol(:I), Symbol(:E)),
+    :exposure_e′=>exposure_petri(Symbol(:exp_e), Symbol(:S), Symbol(:E),Symbol(:E)),
+    :exposure_a′=>exposure_petri(Symbol(:exp_a), Symbol(:S), Symbol(:A),Symbol(:E)),
+    :exposure_i2′=>exposure_petri(Symbol(:exp_i2), Symbol(:S), Symbol(:I2), Symbol(:E))
   ),
   Dict(
-    :s=>ob(Symbol(:S, x)),
-    :e=>ob(Symbol(:E, x)),
-    :a=>ob(Symbol(:A, x)),
-    :i=>ob(Symbol(:I, x)),
-    :i2=>ob(Symbol(:I2, x)),
-    :r=>ob(Symbol(:R, x)),
-    :r2=>ob(Symbol(:R2, x)),
-    :d=>ob(Symbol(:D, x)),
-    :s′=>ob(Symbol(:S, y)),
-    :e′=>ob(Symbol(:E, y)),
-    :a′=>ob(Symbol(:A, y)),
-    :i′=>ob(Symbol(:I, y)),
-    :i2′=>ob(Symbol(:I2, y)),
-    :r′=>ob(Symbol(:R, y)),
-    :r2′=>ob(Symbol(:R2, y)),
-    :d′=>ob(Symbol(:D, y))
+    :s=>ob(Symbol(:S)),
+    :e=>ob(Symbol(:E)),
+    :a=>ob(Symbol(:A)),
+    :i=>ob(Symbol(:I)),
+   :i2=>ob(Symbol(:I2)),
+    :r=>ob(Symbol(:R)),
+    :r2=>ob(Symbol(:R2)),
+    :d=>ob(Symbol(:D)),
+    :s′=>ob(Symbol(:S)),
+    :e′=>ob(Symbol(:E)),
+    :a′=>ob(Symbol(:A)),
+    :i′=>ob(Symbol(:I)),
+    :i2′=>ob(Symbol(:I2)),
+    :r′=>ob(Symbol(:R)),
+    :r2′=>ob(Symbol(:R2)),
+    :d′=>ob(Symbol(:D))
   ));
 
 # Use this new presentation to define a model
@@ -121,61 +193,52 @@ display_uwd(crossexposure)
 # population wires instead of each individual state as a wire. Each of these
 # populations has their own COEXIST model, and interact through cross exposure
 
-bundled_cross(x,y) = bundle_legs(F_cx(crossexposure, x, y), [tuple([1:8;]...), tuple([9:16;]...)])
-bundled_coex(x) = bundle_legs(F(coexist, x), [tuple([1:8;]...)])
-F_tcx(ex) = oapply(ex, Dict(
-    :crossexp12=>bundled_cross(3,4),
-    :crossexp13=>bundled_cross(3,5),
-    :crossexp23=>bundled_cross(4,5),
-    :coex1=>bundled_coex(3),
-    :coex2=>bundled_coex(4),
-    :coex3=>bundled_coex(5)));
-
-threeNCoexist = @relation (pop1, pop2, pop3) begin
-    crossexp12(pop1, pop2)
-    crossexp13(pop1, pop3)
-    crossexp23(pop2, pop3)
-    coex1(pop1)
-    coex2(pop2)
-    coex3(pop3)
-end;
-display_uwd(threeNCoexist)
-#-
-threeNCoexist_algpetri = apex(F_tcx(threeNCoexist))
-Graph(threeNCoexist_algpetri);
-save_fig(Graph(threeNCoexist_algpetri), "3ncoexist_petri", "svg"); # hide
 
 # ![3-generation COEXIST model petri net](3ncoexist_petri.svg)
 
 # We can JSON to convert this Petri net into an
 # easily shareable format
 
-JSON.print(threeNCoexist_algpetri.tables)
 
-# We can now easily generate a solver for DifferentialEquations.jl
-# because we encoded the intitial parameters and rates throughout
-# the construction of the model, the final result knows its
-# concentrations and rates.
+""" diff_strat(epi_model, connection_graph)
 
-tspan = (0.0,100.0);
-prob = ODEProblem(vectorfield(threeNCoexist_algpetri),concentrations(threeNCoexist_algpetri),tspan,rates(threeNCoexist_algpetri));
-sol = solve(prob,Tsit5());
-plot(sol, xlabel="Time", ylabel="Number of people")
+  This function takes in a LabelledPetriNet and a graph which describes
+  geographical connections. It returns a LabelledPetriNet which models
+  diffusion between geographic populations described by the given graph.
+"""
 
-# If we want to model other intervention methods,
-# we can simply adjust the rates of exposure to
-# represent stay at home orders and mask wearing.
-# Because of how we have defined our rates, we can
-# simply update the social mixing rates, and
-# resolve the model.
-
-for i in 1:length(social_mixing_rate)
-  for j in 1:length(social_mixing_rate[1])
-    social_mixing_rate[i][j] = social_mixing_rate[i][j] / (i != j ? 10 : 5);
-  end
+function diff_strat(epi_model::LabelledPetriNet, connection_graph::Catlab.Graphs.BasicGraphs.Graph)
+    epi_func(ind) = bundle_legs(Open(add_index(epi_model, ind)), [tuple([1:ns(epi_model);]...)])
+    conn(x, y) = bundle_legs(Open(diff_connection(epi_func, x, y)), [tuple([1:ns(epi_model);]...), tuple([(ns(epi_model)+1):(2*ns(epi_model));]...)])
+    stratify(epi_func, connection_graph, conn)
 end
-threeNCoexist_algpetri = apex(F_tcx(threeNCoexist));
 
-prob = ODEProblem(vectorfield(threeNCoexist_algpetri),concentrations(threeNCoexist_algpetri),tspan,rates(threeNCoexist_algpetri));
-sol = solve(prob,Tsit5());
-plot(sol, xlabel="Time", ylabel="Number of people")
+""" dem_strat(epi_model, connection_graph, sus_state, exp_state, inf_states)
+
+  This function takes in a LabelledPetriNet and a graph which describes
+  infection connections between populations. It also takes in the symbol used
+  for susceptible states, the symbol used for the exposed state, and an array
+  of symbols for states that can expose susceptible states. It returns a
+  LabelledPetriNet which models diffusion between populations described by the
+  given graph.
+"""
+
+function dem_strat(epi_model::LabelledPetriNet, connection_graph::Catlab.Graphs.BasicGraphs.Graph, sus_state::Symbol, exp_state::Symbol, inf_states::Array{Symbol})
+    epi_func(ind) = bundle_legs(Open(add_index(epi_model, ind)), [tuple([1:ns(epi_model);]...)])
+    conn(x, y) = bundle_legs(Open(
+                    dem_connection(epi_func, sus_state::Symbol, exp_state::Symbol,
+                                    inf_states::Array{Symbol}, x, y)),
+                    [tuple([1:ns(epi_model);]...), tuple([(ns(epi_model)+1):(2*ns(epi_model));]...)])
+    stratify(epi_func, connection_graph, conn)
+end
+
+cycle_3 = Catlab.Graphs.BasicGraphs.Graph(3)
+add_edges!(cycle_3, [1,2,3], [2,3,1])
+
+coex = apex(F(coexist));
+
+coex_dem = apex(dem_strat(coex, cycle_3, :S, :E, [:I, :E, :I2, :A]));
+coex_diff = apex(diff_strat(coex, cycle_3))
+
+save_fig(AlgebraicPetri.Graph(coex_dem), "coex_crx", "svg");
+save_fig(AlgebraicPetri.Graph(coex_diff), "coex_diff", "svg");
