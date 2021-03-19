433eb2a62048f5eaf90c5cbba3871ddd30e606b9
bosonbaas <bosonbaas@gmail.com>
2021-03-19 10:22:26 -0400

    Added docstrings

--- src/CatalystInterop.jl
+++ src/CatalystInterop.jl
@@ -1,3 +1,9 @@
+""" Supports conversion from PetriNets to Catalyst ReactionSystems
+
+This provides access to the parameter estimation, optimization, and sensitivity
+tooling provided in the Catalyst library
+"""
+
 module CatalystInterop
   using AlgebraicPetri
   using Catlab.CategoricalAlgebra
@@ -6,6 +12,13 @@ module CatalystInterop
 
   counter(a) = [count(==(i),a) for i in unique(a)]
 
+  """ Convert a general PetriNet to a ReactionSystem
+
+  This conversion forgets any labels or rates provided, and converts all
+  parameters and variables into symbols. It does preserve the ordering of
+  transitions and states though (Transition 1 has a rate of k[1], state 1 has a
+  concentration of S[1])
+  """
   function ReactionSystem(pn::AbstractPetriNet)
     @parameters t k[1:nt(pn)]
     @variables S[collect(1:ns(pn))](t)
