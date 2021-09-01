e111604334b83bbd8e4d000e853b2f84ceadd747
bosonbaas <bosonbaas@gmail.com>
2021-09-01 16:06:36 -0400

    Added test for subgraphs of subgraphs

--- test/petri.jl
+++ test/petri.jl
@@ -1,3 +1,6 @@
+using Catlab.Graphs
+using Catlab.Graphics.Graphviz
+
 f = Open([1, 2], PetriNet(4, (1,3), (2,4)), [3, 4])
 
 g = Open([1,2], PetriNet(3, ((1,2),3)), [3])
@@ -38,3 +41,17 @@ lrn′ = Open([:I], LabelledReactionNet{Number,Int}([:I=>10, :R=>0], ((:rec=>.25
 
 death_petri = Open(PetriNet(1, 1=>()));
 @test Graph(death_petri) isa Graph
+
+# Test visualization of nested subgraphs
+SIR  = LabelledReactionNet{Float64, Float64}([:S=>1.0, :I=>0.0, :R=>0.0],
+                                             (:inf=>0.5)=>((:S,:I)=>(:I,:I)),
+                                             (:rec=>0.1)=>(:I=>:R))
+stmts1 = Vector{Statement}([AlgebraicPetri.Subgraph(SIR; pre="1_")])
+graph_attrs = Attributes(:rankdir=>"LR")
+node_attrs  = Attributes(:shape=>"plain", :style=>"filled", :color=>"white")
+edge_attrs  = Attributes(:splines=>"splines")
+g = Graphviz.Digraph("G", stmts1; graph_attrs=graph_attrs, node_attrs=node_attrs, edge_attrs=edge_attrs)
+stmts2 = Vector{Statement}([AlgebraicPetri.Subgraph(g; post="_2")])
+g2 = Graphviz.Digraph("G", stmts2; graph_attrs=graph_attrs, node_attrs=node_attrs, edge_attrs=edge_attrs)
+@test g2 isa Graph
+@test g2.stmts[1].stmts[1].stmts[1].name == "1_s1_2"
