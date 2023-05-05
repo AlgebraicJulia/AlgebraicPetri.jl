1beeffe50641322c6d560867d0af49b86bfe1041
p-stokes <patrick.stokes@gmail.com>
2023-05-05 01:20:07 -0400

    Began moving new mca functions from test to src

--- src/SubACSets.jl
+++ src/SubACSets.jl
@@ -1,6 +1,7 @@
 module SubACSets
 export mca
 
+using DataStructures
 using Catlab, Catlab.Theories, Catlab.CategoricalAlgebra, Catlab.Graphs, Catlab.CategoricalAlgebra.FinSets
 using Catlab.Graphics
 using Catlab.CategoricalAlgebra.Diagrams
@@ -26,24 +27,50 @@ function normalize_order(X::ACSet, Y::ACSet)
   size(X) ≤ size(Y) ? (X,Y) : (Y,X)
 end
 
-# given an acset X: C→Set and an object c ∈ C, compute all possible X' ↪ X which
-# are given by removing a single element from the set Xc
-function one_removed_subobs(X::ACSet, c)
-  C    = acset_schema(X)
-  subs = [filter(j -> j != i, parts(X,c)) for i ∈ parts(X,c)]
-  build_sub(sub) = hom(
-    # need double negation to remove dangling edges
-    ¬¬Subobject(
-      X,
-      NamedTuple(
-        zip(
-          objects(C),
-          [o == c ? sub : parts(X, o) for o ∈ objects(C)]
-        )
-      )
-    )
-  )
-  map(build_sub, subs)
+"""Recursively delete anything, e.g. deleting a vertex deletes its edges"""
+function rm_cascade_subobj(X::ACSet, rm_subs)
+  S    = acset_schema(X)
+  subs = Dict([k=>Set(parts(X,k)) for k in objects(S)])
+  rm_subs = Dict([k=>Set(v) for (k,v) in pairs(rm_subs)])
+  # println(keys(subs))
+  while !isempty(rm_subs)
+    # println(keys(rm_subs))
+    curr_c = first(rm_subs)[1]
+    if isempty(rm_subs[curr_c])
+        delete!(rm_subs,curr_c)
+    else
+        # println(rm_subs[curr_c])
+        curr_part = pop!(rm_subs[curr_c])
+        if curr_part in subs[curr_c]
+            delete!(subs[curr_c],curr_part)
+            #
+            # if isempty(subs[curr_c])
+            #     delete!(subs,curr_c)
+            # end
+            # 
+            for (f,c,d) in homs(S)
+                if d==curr_c && c in keys(subs)
+                    # println("Made it")
+                    for test_part in subs[c]
+                        if X[test_part,f] == curr_part
+                            if c in keys(rm_subs)
+                                push!(rm_subs[c], test_part)
+                            else
+                                rm_subs[c] = Set([test_part])
+                            end
+                        end
+                    end
+                end
+            end
+        end
+        if isempty(rm_subs[curr_c])
+            delete!(rm_subs,curr_c)
+        end
+    end
+  end
+  # return subs # dom(hom(Subobject(X,NamedTuple(subs))))
+  # return Dict([k => collect(v) for (k,v) in pairs(subs)])
+  return dom(hom(Subobject(X,NamedTuple(Dict([k => collect(v) for (k,v) in pairs(subs)])))))
 end
 
 """
@@ -57,16 +84,26 @@ function size(X::ACSet)
   foldl(+, [length(parts(X, oₛ)) for oₛ ∈ objects(acset_schema(X))])
 end
 
+function strip_attributes(p::ACSet; attributes::Vector{Symbol}=Symbol[])
+  attributes = isempty(attributes) ? attrtypes(acset_schema(p)) : attributes
+  isempty(attributes) ? p : map(p; Dict(attr=>(x->nothing) for attr in attributes)...)
+end
+
+function strip_attributes(p::ACSetTransformation; kw...)
+  init = NamedTuple([k=>collect(v) for (k,v) in pairs(components(p))])
+  homomorphism(strip_attributes(dom(p); kw...), strip_attributes(codom(p); kw...), initial=init)
+end
+
 
 """Get all monomorphisms from an acset X to an acset Y
 """
 monos(X::ACSet, Y::ACSet) =
-  homomorphism(X, strip_names(Y); monic = true, type_components=(Name=x->nothing,),)
+  homomorphism(X, strip_attributes(Y); monic = true, type_components=(Name=x->nothing,),)
 
 """Ask: "does there exists a mono X ↪ Y ?"
 """
 exists_mono(X::ACSet,Y::ACSet)::Bool =
-  is_homomorphic(X, strip_names(Y); monic = true, type_components=(Name=x->nothing,),)
+  is_homomorphic(X, strip_attributes(Y); monic = true, type_components=(Name=x->nothing,),)
 
 
 """Brute-force implementation of Maximum Common Acset (MCA).
@@ -75,22 +112,33 @@ Task : find all a with with |a| maximum possible such that there is a monic span
 """
 function mca(XX::ACSet, YY::ACSet)
   (X,Y) = normalize_order(XX,YY)
-  exists_mono(X,Y) ? [X] : mca_help(X, Y)
+  hX = BinaryHeap(Base.By(size,Base.Order.Reverse),[X])
+  return mca_help([],hX,Y)
 end
 
-function mca_help(X::ACSet, Y::ACSet)
-  C = acset_schema(X) #X: C → Set
-  getsmaller(Z,c) = map(dom, one_removed_subobs(Z, c))
-  # enumerate all sub-acsets X' ↪ X of the acset X: C → Set obtained by removing one point from Xc for some c ∈ C
-  oneRemovedFromX = concatmap(γ -> getsmaller(X,γ), filter(c -> !isempty(parts(X,c)), objects(C)))
-  # keep only those X' ↪ X such that there exists mono X' ↪ Y
-  Y_subs          = filter(χ -> exists_mono(χ, Y), oneRemovedFromX)
-  # either terminate or recurse
-  if isempty(Y_subs)
-    concatmap(χ -> mca_help(χ, Y), oneRemovedFromX)
-  else
-    ω = maximum(map(size,Y_subs))
-    filter(y -> size(y) == ω, Y_subs)
+function mca_help(mca_list, X_subs, Y)
+  if !isempty(X_subs) && (isempty(mca_list) || size(mca_list[1]) <= size(first(X_subs)))
+    curr_X_sub = pop!(X_subs)
+    C = acset_schema(curr_X_sub) #X: C → Set
+    if exists_mono(curr_X_sub,Y) 
+      if length(filter(x -> curr_X_sub==x, mca_list))==0
+          push!(mca_list,curr_X_sub)
+      end
+    else
+      indiv_parts = []
+      for c in objects(C)
+          for p in parts(curr_X_sub,c)
+              push!(indiv_parts, NamedTuple([c => [p]]))
+          end
+      end
+      new_X_subs = concatmap(γ -> rm_cascade_subobj(curr_X_sub,γ), indiv_parts)
+      for new_sub in new_X_subs
+          push!(X_subs,new_sub)
+      end
+    end
+    return mca_help(mca_list, X_subs, Y)
+  else # isempty(X_subs) || AlgebraicPetri.SubACSets.size(mca_list[1]) > AlgebraicPetri.SubACSets.size(first(X_subs))
+    return mca_list
   end
 end
 
