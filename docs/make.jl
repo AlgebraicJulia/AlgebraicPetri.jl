08e44839975a70b14f291839aa801fe948d5999e
Micah Halter <micah@mehalter.com>
2023-05-09 10:56:02 -0400

    refactor(docs): move examples to docs and clean up random environments
    Signed-off-by: Micah Halter <micah@mehalter.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,33 @@
 using Documenter
 using Literate
 
+const literate_dir = joinpath(@__DIR__, "literate")
+const generated_dir = joinpath(@__DIR__, "src", "generated")
+
 @info "Loading AlgebraicPetri"
 using AlgebraicPetri
 
-# Set Literate.jl config if not being compiled on recognized service.
-config = Dict{String,String}()
-if !(haskey(ENV, "GITHUB_ACTIONS") || haskey(ENV, "GITLAB_CI"))
-  config["nbviewer_root_url"] = "https://nbviewer.jupyter.org/github/AlgebraicJulia/AlgebraicPetri.jl/blob/gh-pages/dev"
-  config["repo_root_url"] = "https://github.com/AlgebraicJulia/AlgebraicPetri.jl/blob/master/docs"
-end
+const no_literate = "--no-literate" in ARGS
+if !no_literate
+  @info "Building Literate.jl docs"
 
-const literate_dir = joinpath(@__DIR__, "..", "examples")
-const generated_dir = joinpath(@__DIR__, "src", "examples")
+  # Set Literate.jl config if not being compiled on recognized service.
+  config = Dict{String,String}()
+  if !(haskey(ENV, "GITHUB_ACTIONS") || haskey(ENV, "GITLAB_CI"))
+    config["nbviewer_root_url"] = "https://nbviewer.jupyter.org/github/AlgebraicJulia/AlgebraicPetri.jl/blob/gh-pages/dev"
+    config["repo_root_url"] = "https://github.com/AlgebraicJulia/AlgebraicPetri.jl/blob/master/docs"
+  end
 
-for (root, dirs, files) in walkdir(literate_dir)
-  out_dir = joinpath(generated_dir, relpath(root, literate_dir))
-  for file in files
-    f,l = splitext(file)
-    if l == ".jl" && !startswith(f, "_")
-      Literate.markdown(joinpath(root, file), out_dir;
-        config=config, documenter=true, credit=false)
-      Literate.notebook(joinpath(root, file), out_dir;
-        execute=true, documenter=true, credit=false)
+  for (root, dirs, files) in walkdir(literate_dir)
+    out_dir = joinpath(generated_dir, relpath(root, literate_dir))
+    for file in files
+      f,l = splitext(file)
+      if l == ".jl" && !startswith(f, "_")
+        Literate.markdown(joinpath(root, file), out_dir;
+          config=config, documenter=true, credit=false)
+        Literate.notebook(joinpath(root, file), out_dir;
+          execute=true, documenter=true, credit=false)
+      end
     end
   end
 end
@@ -39,12 +44,12 @@ makedocs(
   pages     = Any[
     "AlgebraicPetri.jl" => "index.md",
     "Examples" => Any[
-      "examples/predation/lotka-volterra.md",
-      "examples/covid/epidemiology.md",
-      "examples/covid/coexist/coexist.md",
-      "examples/enzymes/enzyme_reactions.md",
-      "examples/covid/bilayerconversion.md",
-      "examples/covid/stratification/stratification.md",
+      "generated/predation/lotka-volterra.md",
+      "generated/covid/epidemiology.md",
+      "generated/covid/coexist/coexist.md",
+      "generated/enzymes/enzyme_reactions.md",
+      "generated/covid/bilayerconversion.md",
+      "generated/covid/stratification/stratification.md",
     ],
     "Library Reference" => "api.md",
   ]
