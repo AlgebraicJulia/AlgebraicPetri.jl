fa2dfd1d92124558ec2c20386014859ea77bee5a
Matt Cuffaro <cuffaro.matt@gmail.com>
2023-12-06 12:34:45 -0500

    mpi test

---
+++ job.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+julia --project tests/runtests.jl > log_test.txt
+
---
+++ jobscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=daemon_job_test    # Job name
+#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=cuffaro.m@ufl.edu # Where to send mail	
+#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --mem=1gb                     # Job memory request
+#SBATCH --time=00:05:00               # Time limit hrs:min:sec
+#SBATCH --output=daemon_test_%j.log   # Standard output and error log
+pwd; hostname; date
+
+module load gcc/9.3.0 openmpi/4.1.5 julia
+
+echo "Running tests on a single CPU core"
+
+srun --mpi=$HPC_PMIX ./job.sh
+
+date
+
+
