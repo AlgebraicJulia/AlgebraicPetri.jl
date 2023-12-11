0e83d4a9c904db6d7f9acac3e15dcdb30a2ee1db
Matt Cuffaro <cuffaro.matt@gmail.com>
2023-12-11 08:49:06 -0500

    testing with agent loading julia in pipeline

--- jobscript.sh
+++ jobscript.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=cuffaro.m@ufl.edu # Where to send mail	
 #SBATCH --ntasks=1                    # Run on a single CPU
-#SBATCH --mem=5gb                     # Job memory request
+#SBATCH --mem=8gb                     # Job memory request
 #SBATCH --time=00:05:00               # Time limit hrs:min:sec
 #SBATCH --output=daemon_test_%j.log   # Standard output and error log
 pwd; hostname; date
