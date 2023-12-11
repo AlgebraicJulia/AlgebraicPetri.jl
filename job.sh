abe85fea9b5cc8f375eb74885ed14795161b9846
Matt Cuffaro <cuffaro.matt@gmail.com>
2023-12-11 10:57:15 -0500

    testing with agent loading julia in pipeline

--- job.sh
+++ job.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-julia --project -e 'using Pkg; Pkg.instantiate(;verbose=true)'
-julia --project -e 'using Pkg; Pkg.precompile'
-julia --project -e 'using Pkg; Pkg.status'
-
-
-julia --project test/runtests.jl > log_test.md
+julia --project -e 'using Pkg; Pkg.status; Pkg.test()' > log_test.md
