6ebe1ece0cef0a0a230909adb087f09362384951
Matt Cuffaro <cuffaro.matt@gmail.com>
2023-12-14 13:23:20 -0500

    testing docs build

--- jobscript.sh
+++ jobscript.sh
@@ -13,7 +13,7 @@ module load gcc/12.2.0 openmpi/4.1.5 julia
 echo "Running some tests!?! webhook updated!! ahh!! ah!!!"
 
 julia --project=docs/ -e 'using Pkg; Pkg.status()'
-julia --project=docs/ -e 'using Pkg; Pkg.instantiate(); include("docs/make.jl")'
+julia --project=docs/ -e 'using Pkg; include("docs/make.jl")'
 # julia --project -e 'using Pkg; Pkg.status; Pkg.test()' > log_test.md
 # ./job.sh
 
